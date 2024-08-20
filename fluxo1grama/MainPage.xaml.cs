d03cacf41fe8c2438aa0874007b2babc54bba8ef
fluxo1grama <victoriaferjc@gmail.com>
2024-08-20 11:52:43 -0300

    20/08/2024

--- fluxo1grama/MainPage.xaml.cs
+++ fluxo1grama/MainPage.xaml.cs
@@ -1,18 +1,50 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System.Diagnostics.Tracing;
+using Microsoft.Maui.Controls;
+using Vision;
 
 namespace fluxo1grama
-{
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private async void OnNextPageClicked(object sender, EventArgs e)
+        public void botaocliente(object sender, EventArgs args)
         {
-            // Navegar para a próxima página
-            await Navigation.PushAsync(new NextPage());
+            Application.Currente.Mainpage = new Cliente;
+
+        } 
+        public void botaomateriaprima(object sender, EventTags args)
+        {
+            Application.Current.MainPage = new Materiaprima();
+            
         }
+        public void botaofornecedor(object sender, EventTags args)
+        {
+            Application.Current.MainPage = new Fornecedor();
+        }
+        public void botaopedido(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new Pedido()
+        }
+        public void botaoalmoxarifado(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new Almoxarifado()
+        }
+        public void botaoformadepagamento(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new Formadepagamento
+        }
+        public void botaoprodutos(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new Produtos
+        }
+        public void botaosetores(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new Setores
+        }
+        
+
     }
-}
+
